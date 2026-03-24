dc6f1dab30c91b7ef3f1866af53d2184a1a06db8
SiGMono <91075739+SiGMono@users.noreply.github.com>
2026-03-24 14:45:47 +0100

    fin

--- 2026_03_17_Lab1/app.cpp
+++ 2026_03_17_Lab1/app.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -19,9 +20,10 @@ class Kwadrat : public Figura{
         int obliczObwod() override {
             return 4*a;
         }
-        Kwadrat(int a){
-            this->a=a;
-        }
+
+    Kwadrat(int a){
+        this->a=a;
+    }
 };
 
 class Kolo : public Figura{
@@ -34,9 +36,46 @@ class Kolo : public Figura{
         int obliczObwod() override{
             return 3.13*r*r;
         }
-        Kolo(int r){
-            this->r=r;
+
+    Kolo(int r){
+        this->r=r;
+    }
+};
+
+class Prostokat : public Figura{
+    private:
+        int a,b;
+    public:
+        int obliczPole() override{
+            return a*b;
         }
+        int obliczObwod() override{
+            return 2*a + 2*b;
+        }
+    
+    Prostokat(int a,int b){
+        this->a=a;
+        this->b=b;
+    }
+};
+
+class Trojkat : public Figura{
+    private:
+        int a,b,c;
+    public:
+        int obliczPole() override{
+            int p = ((a+b+c)/2);
+            return sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        int obliczObwod() override{
+            return a+b+c;
+        }
+
+    Trojkat(int a, int b, int c){
+        this->a=a;
+        this->b=b;
+        this->c=c;
+    }
 };
 
 
@@ -45,6 +84,8 @@ int main(int argc, char const *argv[])
 {
     Kwadrat *kwadrat1 = new Kwadrat(4);
     Kolo *kolo1 = new Kolo(3);
+    Prostokat *prostokat1 = new Prostokat(3,4);
+    Trojkat *trojkat1 = new Trojkat(2,3,4);
     Figura *user;
 
     int decyzja;
@@ -53,14 +94,39 @@ int main(int argc, char const *argv[])
         cout<<"     <---------->     "<<endl;
         cout<<"1 - Kolo"<<endl;
         cout<<"2 - Kwadrat"<<endl;
+        cout<<"3 - Prostokat"<<endl;
+        cout<<"4 - Trojkat"<<endl;
         cout<<"5 - pole figury"<<endl;
         cout<<"6 - obwod figury"<<endl;
         cout<<"0 - exit"<<endl;
 
-        if(decyzja == 1){ user = kolo1; }
-        if(decyzja == 2){ user = kwadrat1; }
-        if(decyzja == 5){ user -> obliczPole();}
-        if(decyzja == 6){ user -> obliczObwod(); }
+        switch(decyzja){
+            case 1:
+                user = kolo1;
+                break;
+            case 2:
+                user = kwadrat1;
+                break;
+            case 3:
+                user = prostokat1;
+                break;
+            case 4:
+                user = trojkat1;
+                break;
+            case 5:
+                if(user != nullptr) cout<<"Pole to: "<<user->obliczPole();
+                else cout<<"Najpierw wybierz figure";
+                break;
+            case 6:
+                if(user != nullptr) cout<<"Obwod to: "<<user->obliczObwod();
+                else cout<<"Najpierw wybierz figure";
+                break;
+            case 0:
+                break;
+            default:
+                cout<<"Nieznana opcja";
+                break;
+        }
 
     }while(decyzja != 0);
 
